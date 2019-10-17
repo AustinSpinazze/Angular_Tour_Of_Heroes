669cf74d593469511658c2b8c661c519e5e12151
Austin Spinazze <spinazzeaustin@yahoo.com>
2019-10-17 17:28:00 -0500

    App now displays list creating by binding to hero object
    Used *ngFor to display list, *ngIf to conditionally include or exclude a block of HTML, added constant array of heroes to and exported as class

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'; // Imports hero object from hero.ts so we can make multiple heroes
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice'},
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Batty Man'},
+    { id: 14, name: 'Barnacle Boy'},
+    { id: 15, name: 'Mermaid Man'},
+    { id: 16, name: 'Raptor'},
+    { id: 17, name: 'The Tornado'},
+    { id: 18, name: 'Patrick Star'},
+    { id: 19, name: 'Doggo McDoggyface'},
+    { id: 20, name: 'Dr. Kumar'}
+];
